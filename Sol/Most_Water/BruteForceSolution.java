02bd0a82f015126f0f04f8cb61f01505a27d122d
henry <njjza@hotmail.com>
2021-05-03 19:55:08 -0700

    Change Project Tree

---
+++ Sol/Most_Water/BruteForceSolution.java
@@ -0,0 +1,31 @@
+package Most_Water;
+
+import java.util.*;
+public class BruteForceSolution {
+    public static int areaCalculation(int[] bars) {
+        return 
+    }
+    public static int solution(int[] bars) {
+        int start, end, area = bars[0]
+        int past = area;
+
+        for(int i = 1, i < bars.length; i++ ) {
+            past = (area > past) ? past;
+            bar_cur = bars[i];
+
+        }
+
+        return 
+    }
+    
+    public static void main(String[] args) {
+        int[] case1_height = {1,1}
+        int case1_out = 1
+        int[] case2_height = [1,8,6,2,5,4,8,3,7]
+        int case2_out = 49
+        int[] case3_height = [4,3,2,1,4]
+        int case3_out = 16
+        int[] case4_height = [1,2,1]
+        int case4_out = 2
+    }
+}
